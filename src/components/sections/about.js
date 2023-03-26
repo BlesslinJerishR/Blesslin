5f89b752b46f4639edbb5be3507e4d7a8e3f6ccf
Mastermind <mastermindx33@gmail.com>
2023-03-26 12:42:12 +0530

    Add about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,35 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'HTML',
+    'Bootstrap',
+    'CSS',
+    'Tailwind',
+    'JavaScript (ES6+)',
+    'Typescript',
+    'React',
+    'Redux',
+    'NextJS',
+    'NodeJS',
+    'Python',
+    'Django',
+    'Ruby',
+    'Ruby on Rails',
+    'C#',
+    '.NET',
+    'Azure',
+    'AWS',
+    'Firebase',
+    'Netlify',
+    'PostgreSQL',
+    'Java',
+    'SQL',
+    'MongoDB',
+    'Express',
+    'React Native',
+    'VueJS',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,33 +163,23 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello ! My name is Blesslin Jerish R and I enjoy creating things that live on the
+              internet. My interest in web development started back in 2016 when I decided to try
+              editing custom websites themes — turns out hacking together a custom inspect element
+              button taught me a lot about HTML &amp; CSS!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://www.zoho.com/">a huge corporation</a>,{' '}
+              <a href="https://www.ecanarys.com/">a IT company</a>,{' '}
+              <a href="https://onsepro.com/">a start-up</a> and{' '}
+              <a href="https://www.microverse.org/">a remote coding school</a>. My main focus these
+              days is building accessible, inclusive projects and Full-Stack Web Applications at{' '}
+              <a href="https://upstatement.com/">Microverse</a>.
             </p>
 
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few technologies I’ve been working with recently :</p>
           </div>
 
           <ul className="skills-list">
